1ef65f7353d52753a79206a70d8aa58ff037d8f0
Jakubem <jakubmx@gmail.com>
2018-07-29 11:37:53 +0200

    add matching hover on nav arrows

--- js/components/ArrowNext.vue
+++ js/components/ArrowNext.vue
@@ -1,7 +1,7 @@
 <template>
-  <svg class="nav-arr" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M0 12L8.5 6L0 0V12ZM2 3.86L5.03 6L2 8.14V3.86Z" transform="translate(6 6)" fill="#23221F" />
-    <path d="M2 0H0V12H2V0Z" transform="translate(16 6)" fill="#23221F" />
+  <svg fill="#23221F" class="nav-arr" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 12L8.5 6L0 0V12ZM2 3.86L5.03 6L2 8.14V3.86Z" transform="translate(6 6)" />
+    <path d="M2 0H0V12H2V0Z" transform="translate(16 6)" />
   </svg>  
 </template>
 
--- js/components/ArrowPrev.vue
+++ js/components/ArrowPrev.vue
@@ -1,7 +1,7 @@
 <template>
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M2 0H0V12H2V0Z" transform="translate(6 6)" fill="#23221F" />
-    <path d="M0 6L8.5 12V0L0 6ZM6.5 8.14L3.47 6L6.5 3.86V8.14Z" transform="translate(9.5 6)" fill="#23221F" />
+  <svg fill="#23221F" class="nav-arr" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2 0H0V12H2V0Z" transform="translate(6 6)" />
+    <path d="M0 6L8.5 12V0L0 6ZM6.5 8.14L3.47 6L6.5 3.86V8.14Z" transform="translate(9.5 6)" />
   </svg>
 </template>
 
