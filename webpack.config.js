f64f179336ac76273852cdd1f15cf3923171c370
Jakubem <jakubmx@gmail.com>
2018-07-30 20:40:32 +0200

    minify html templates

--- webpack.config.js
+++ webpack.config.js
@@ -4,81 +4,99 @@ const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const minify = {
+    removeComments: true,
+    collapseWhitespace: true,
+    removeRedundantAttributes: true,
+    useShortDoctype: true,
+    removeEmptyAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    keepClosingSlash: true,
+    minifyJS: true,
+    minifyCSS: true,
+    minifyURLs: true,
+}
+
+
 module.exports = {
-  entry: './js/main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-          }
-          // other vue-loader options go here
+    entry: './js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {}
+                    // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.(png|jpg|gif|svg|ttf)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: "css-loader" },
+                    { loader: "sass-loader" }
+                ],
+            },
+        ]
+    },
+    stats: 'errors-only',
+    resolve: {
+        extensions: ['.ts', '.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
         }
-      },
-      {
-        test: /\.(png|jpg|gif|svg|ttf)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {loader: "css-loader"},
-          {loader: "sass-loader"}
-      ],
-      },
+    },
+    plugins: [
+        new FriendlyErrorsWebpackPlugin({
+            clearConsole: true,
+        }),
+        new HtmlWebpackPlugin({
+            hash: false,
+            minify: minify,
+            template: './index.html',
+            filename: './index.html'
+        }),
+        new HtmlWebpackPlugin({
+            hash: false,
+            minify: minify,
+            collapseWhitespace: true,
+            template: './episodes.html',
+            filename: './episodes.html'
+        }),
+        new HtmlWebpackPlugin({
+            hash: false,
+            minify: minify,
+            template: './about.html',
+            filename: './about.html'
+        }),
+        new HtmlWebpackPlugin({
+            hash: false,
+            minify: minify,
+            template: './episode.html',
+            filename: './episode.html'
+        }),
+        new CopyWebpackPlugin([
+            { from: 'fonts', to: 'fonts' },
+            { from: 'images', to: 'images' },
+            { from: 'js/productbclub.js', to: 'productbclub.js' },
+            // for now
+            { from: 'js/episode.json', to: 'episode.json' },
+        ]),
+        new MiniCssExtractPlugin({
+            filename: "bundle.css",
+        }),
+        new VueLoaderPlugin()
     ]
-  },
-  stats: 'errors-only',
-  resolve: {
-    extensions: ['.ts', '.js', '.vue', '.json'],
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    }
-  },
-  plugins: [
-    new FriendlyErrorsWebpackPlugin({
-        clearConsole: true,
-      }),
-    new HtmlWebpackPlugin({
-      hash: false,
-      template: './index.html',
-      filename: './index.html'
-    }),
-    new HtmlWebpackPlugin({
-      hash: false,
-      template: './episodes.html',
-      filename: './episodes.html'
-    }),
-    new HtmlWebpackPlugin({
-      hash: false,
-      template: './about.html',
-      filename: './about.html'
-    }),
-    new HtmlWebpackPlugin({
-      hash: false,
-      template: './episode.html',
-      filename: './episode.html'
-    }),
-    new CopyWebpackPlugin([
-        { from: 'fonts', to: 'fonts' },
-        { from: 'images', to: 'images' },
-        { from: 'js/productbclub.js', to: 'productbclub.js' },
-        // for now
-        { from: 'js/episode.json', to: 'episode.json' },
-      ]),
-    new MiniCssExtractPlugin({
-        filename: "bundle.css",
-    }),
-    new VueLoaderPlugin()
-  ]
 }
