3f6a6c535feaf3dca29c49a7f747c5635ef98ae6
unknown <jakubmx@gmail.com>
2018-07-31 17:32:30 +0200

    carousel smthng

--- js/components/CardWrapper.vue
+++ js/components/CardWrapper.vue
@@ -1,16 +1,25 @@
 <template>
 <div>
   <div class="episodes-wrapper">
-    <single-card v-for="episode in episodes" :key="episode.no" :obj="episode"></single-card>
+    <carousel :per-page="3">
+      <slide v-for="episode in episodes" :key="episode.no">
+        <single-card :obj="episode"></single-card>
+      </slide>
+    </carousel>
   </div>
 </div>
 </template>
 
 <script>
-import SingleCard from './SingleCard.vue';  
+  import { Carousel, Slide } from 'vue-carousel';
+  import SingleCard from './SingleCard.vue';
+  import ArrowNextCarousel from './svgComponents/ArrowNextCarousel.vue';
+  import ArrowPrevCarousel from './svgComponents/ArrowPrevCarousel.vue';
   export default {
     components: {
        'single-card': SingleCard,
+        Carousel,
+        Slide
     },
     mounted: async function() {
     const data = await fetch("./episode.json");
