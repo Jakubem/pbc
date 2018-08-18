dabafa132c7dc76bf1de9d28939f62877b6e4e49
Jakubem <jakubmx@gmail.com>
2018-08-18 20:03:49 +0200

    register new view

--- src/js/main.js
+++ src/js/main.js
@@ -2,20 +2,17 @@ import Vue from 'vue';
 import '../css/style.scss';
 
 import EpisodeTemplate from './components/EpisodeTemplate.vue';
-import CardWrapper from './components/CardWrapper.vue';
+import allEpisodes from './components/allEpisodes.vue';
 import Home from './components/Home.vue';
 
 if (document.getElementById("app")) {
   const app = new Vue({
     el: "#app",
-    data: {
-      episodes: []
-    },
     components: {
-      'episode-template': EpisodeTemplate,
-      'card-wrapper': CardWrapper,
-      'home': Home,
+      EpisodeTemplate,
       SocialSharing,
+      allEpisodes,
+      Home,
     },
   })
 }
